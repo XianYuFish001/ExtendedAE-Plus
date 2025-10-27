c30693228ebf60157dadaa2fb12d16e11e6687a8
XianYuFish001 <xianyufish001@outlook.com>
2025-10-27 17:56:44 +0800

    hotfix: 解决合并出现的network注册问题

--- src/main/java/com/extendedae_plus/init/ModNetwork.java
+++ src/main/java/com/extendedae_plus/init/ModNetwork.java
@@ -16,31 +16,38 @@ public class ModNetwork {
         registrar.playToClient(SetPatternHighlightS2CPacket.TYPE, SetPatternHighlightS2CPacket.STREAM_CODEC, SetPatternHighlightS2CPacket::handle);
         registrar.playToClient(AdvancedBlockingSyncS2CPacket.TYPE, AdvancedBlockingSyncS2CPacket.STREAM_CODEC, AdvancedBlockingSyncS2CPacket::handle);
         registrar.playToClient(ProvidersListS2CPacket.TYPE, ProvidersListS2CPacket.STREAM_CODEC, ProvidersListS2CPacket::handle);
-        registrar.playToServer(RequestProvidersListC2SPacket.TYPE, RequestProvidersListC2SPacket.STREAM_CODEC, RequestProvidersListC2SPacket::handle);
+        registrar.playToServer(RequestUploadingC2SPacket.TYPE, RequestUploadingC2SPacket.STREAM_CODEC, RequestUploadingC2SPacket::handle);
         registrar.playToClient(SetProviderPageS2CPacket.TYPE, SetProviderPageS2CPacket.STREAM_CODEC, SetProviderPageS2CPacket::handle);
         registrar.playToServer(GlobalToggleProviderModesC2SPacket.TYPE, GlobalToggleProviderModesC2SPacket.STREAM_CODEC, GlobalToggleProviderModesC2SPacket::handle);
         registrar.playToServer(CraftingMonitorJumpC2SPacket.TYPE, CraftingMonitorJumpC2SPacket.STREAM_CODEC, CraftingMonitorJumpC2SPacket::handle);
         registrar.playToServer(CraftingMonitorOpenProviderC2SPacket.TYPE, CraftingMonitorOpenProviderC2SPacket.STREAM_CODEC, CraftingMonitorOpenProviderC2SPacket::handle);
-        registrar.playToServer(OpenProviderUiC2SPacket.TYPE, OpenProviderUiC2SPacket.STREAM_CODEC, OpenProviderUiC2SPacket::handle);
         registrar.playToServer(UploadEncodedPatternToProviderC2SPacket.TYPE, UploadEncodedPatternToProviderC2SPacket.STREAM_CODEC, UploadEncodedPatternToProviderC2SPacket::handle);
         registrar.playToServer(UploadInventoryPatternToProviderC2SPacket.TYPE, UploadInventoryPatternToProviderC2SPacket.STREAM_CODEC, UploadInventoryPatternToProviderC2SPacket::handle);
-        // 新增：JEI 中键打开合成界面 & 无线终端拾取方块物品
-        registrar.playToServer(com.extendedae_plus.network.OpenCraftFromJeiC2SPacket.TYPE,
-                com.extendedae_plus.network.OpenCraftFromJeiC2SPacket.STREAM_CODEC,
-                com.extendedae_plus.network.OpenCraftFromJeiC2SPacket::handle);
-        registrar.playToServer(com.extendedae_plus.network.PickFromWirelessC2SPacket.TYPE,
-                com.extendedae_plus.network.PickFromWirelessC2SPacket.STREAM_CODEC,
-                com.extendedae_plus.network.PickFromWirelessC2SPacket::handle);
-        registrar.playToServer(com.extendedae_plus.network.PullFromJeiOrCraftC2SPacket.TYPE,
-                com.extendedae_plus.network.PullFromJeiOrCraftC2SPacket.STREAM_CODEC,
-                com.extendedae_plus.network.PullFromJeiOrCraftC2SPacket::handle);
-        // 频道卡绑定
-        registrar.playToServer(com.extendedae_plus.network.ChannelCardBindPacket.TYPE,
-                com.extendedae_plus.network.ChannelCardBindPacket.STREAM_CODEC,
-                com.extendedae_plus.network.ChannelCardBindPacket::handle);
-        // 无线收发器频率设置
-        registrar.playToServer(com.extendedae_plus.network.SetWirelessFrequencyC2SPacket.TYPE,
-                com.extendedae_plus.network.SetWirelessFrequencyC2SPacket.STREAM_CODEC,
-                com.extendedae_plus.network.SetWirelessFrequencyC2SPacket::handle);
+
+        registrar.playToServer(OpenCraftFromJeiC2SPacket.TYPE,
+                OpenCraftFromJeiC2SPacket.STREAM_CODEC,
+                OpenCraftFromJeiC2SPacket::handle);
+        registrar.playToServer(PickFromWirelessC2SPacket.TYPE,
+                PickFromWirelessC2SPacket.STREAM_CODEC,
+                PickFromWirelessC2SPacket::handle);
+        registrar.playToServer(PullFromJeiOrCraftC2SPacket.TYPE,
+                PullFromJeiOrCraftC2SPacket.STREAM_CODEC,
+                PullFromJeiOrCraftC2SPacket::handle);
+        registrar.playToServer(ChannelCardBindPacket.TYPE,
+                ChannelCardBindPacket.STREAM_CODEC,
+                ChannelCardBindPacket::handle);
+        registrar.playToServer(SetWirelessFrequencyC2SPacket.TYPE,
+                SetWirelessFrequencyC2SPacket.STREAM_CODEC,
+                SetWirelessFrequencyC2SPacket::handle);
+
+        registrar.playToClient(C2SPacketEncodeFinished.TYPE,
+                C2SPacketEncodeFinished.STREAM_CODEC,
+                C2SPacketEncodeFinished::handle);
+        registrar.playToServer(C2SPacketTargetKeyTriggered.TYPE,
+                C2SPacketTargetKeyTriggered.STREAM_CODEC,
+                C2SPacketTargetKeyTriggered::handle);
+        registrar.playToServer(C2SPacketStoneCuttingID.TYPE,
+                C2SPacketStoneCuttingID.STREAM_CODEC,
+                C2SPacketStoneCuttingID::handle);
     }
 }
