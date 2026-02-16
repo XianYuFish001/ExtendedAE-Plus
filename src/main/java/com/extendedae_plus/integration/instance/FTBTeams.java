e1a652b4a699f9a088dd40fe900146404d2170a2
XianYuFish001 <xianyufish001@outlook.com>
2026-02-16 20:44:06 +0800

    fix: 客户端运行时无法获取server team manager导致npe

--- src/main/java/com/extendedae_plus/integration/instance/FTBTeams.java
+++ src/main/java/com/extendedae_plus/integration/instance/FTBTeams.java
@@ -2,8 +2,11 @@ package com.extendedae_plus.integration.instance;
 
 import com.extendedae_plus.integration.ContextModLoaded;
 import com.extendedae_plus.integration.IntegrationFTBTeams;
+import com.mojang.datafixers.util.Either;
 import dev.ftb.mods.ftbteams.api.FTBTeamsAPI;
 import dev.ftb.mods.ftbteams.api.Team;
+import dev.ftb.mods.ftbteams.api.TeamManager;
+import dev.ftb.mods.ftbteams.api.client.ClientTeamManager;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,7 +18,8 @@ public final class FTBTeams implements IntegrationFTBTeams {
         if (member == null || !ContextModLoaded.ftbTeams.isLoaded())
             return Optional.empty();
 
-        return FTBTeamsAPI.api().getManager().getTeamForPlayerID(member)
+        return ManagerWrapped.instance
+                .getTeamForPlayerID(member)
                 .map(Team::getId);
     }
 
@@ -23,10 +27,39 @@ public final class FTBTeams implements IntegrationFTBTeams {
         if (uuid == null || !ContextModLoaded.ftbTeams.isLoaded())
             return Component.empty();
 
-        var manager = FTBTeamsAPI.api().getManager();
+        var manager = ManagerWrapped.instance;
         var team = manager.getTeamByID(uuid);
         if (team.isEmpty()) team = manager.getTeamForPlayerID(uuid);
         if (team.isEmpty()) return Component.empty();
         return team.get().getColoredName();
+    }
+
+    private enum ManagerWrapped {
+        instance;
+
+        private @Nullable Either<TeamManager, ClientTeamManager> manager;
+
+        private Optional<Either<TeamManager, ClientTeamManager>> manager() {
+            if (manager != null) return Optional.of(manager);
+            var api = FTBTeamsAPI.api();
+            manager = api.isManagerLoaded() ? Either.left(api.getManager())
+                    : (api.isClientManagerLoaded() ? Either.right(api.getClientManager())
+                    : null);
+            return Optional.ofNullable(manager);
+        }
+
+        private Optional<Team> getTeamForPlayerID(@Nullable UUID member) {
+            return this.manager().flatMap(it -> it.map(
+                    left -> left.getTeamForPlayerID(member),
+                    right -> Optional.empty()
+            ));
+        }
+
+        private Optional<Team> getTeamByID(@Nullable UUID uuid) {
+            return this.manager().flatMap(it -> it.map(
+                    left -> left.getTeamByID(uuid),
+                    right -> right.getTeamByID(uuid)
+            ));
+        }
     }
 }
